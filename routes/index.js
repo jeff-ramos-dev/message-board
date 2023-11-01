2672efeb31c03a5099f5c65fefc799697ef71e90
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-11-01 16:12:27 -0700

    Pass messages to index page, create get and post routes for new message page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const messages = [{
   text: "Hi there!",
-  user: "Amando",
+  user: "Armando",
   added: new Date()
 },
 {
@@ -15,7 +15,23 @@ const messages = [{
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+/* GET new message form */
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: "New Message"});
+});
+
+/* Receive POST request form form */
+router.post('/new', function(req, res, next) {
+  console.log(req.body['message-author'], req.body['message-text'])
+  messages.push({
+    text: req.body['message-text'],
+    user: req.body['message-author'],
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
